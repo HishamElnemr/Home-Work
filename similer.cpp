f58c131744ea9c4b7799dfaae40bd3b18b5ad898
HishamElnemr <158576358+HishamElnemr@users.noreply.github.com>
2024-02-02 18:11:54 +0200

    Update similer.cpp

--- similer.cpp
+++ similer.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
-#include <cctype>
-#include <vector>
-#include <regex>
 #include <string>
-#include <bitset>
-#include <array>
-#include <regex>
-#include <algorithm>
-#include <cmath>
 using namespace std;
 int main()
 {
