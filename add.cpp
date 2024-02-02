4ac9b6e2ada570109a153ed920782c8412051b9d
HishamElnemr <158576358+HishamElnemr@users.noreply.github.com>
2024-02-02 17:59:33 +0200

    add.cpp

--- add.cpp
+++ add.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <cctype>
 #include <vector>
-#include <regex>
-#include <string>
-#include <bitset>
-#include <array>
-#include <regex>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 int main()
 {
